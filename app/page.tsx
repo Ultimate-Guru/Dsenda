d65ac90c7fecc37086b9dfab15b4c15c09c18e77
Oluwatosin <tosindaniel360@gmail.com>
2026-07-25 19:44:38 +0100

    feat: update layout and home page structure; add request quote page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,27 @@
 // import Image from "next/image";  
 
-import Footer from "./components/Footer";
-import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import DataPotential from "@/components/DataPotential";
+import Features from "@/components/Features";
+import ChooseUs from "@/components/ChooseUs";
+import Process from "@/components/Process";
+import FAQ from "@/components/FAQ";
+import Banner from "@/components/Banner";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
-    <>
+    <main className="min-h-screen overflow-x-hidden">
       <Navbar />
       <Hero />
+      <DataPotential />
+      <Features />
+      <ChooseUs />
+      <Process />
+      <FAQ />
+      <Banner />
       <Footer />
-    </>
+    </main>
   );
 }
