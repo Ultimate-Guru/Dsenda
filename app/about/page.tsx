fb446c3e6d4c1733ea763f6fcad4e57628d73a60
Oluwatosin <tosindaniel360@gmail.com>
2026-07-25 23:37:09 +0100

    feat: update About page layout to include main content components

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import AboutHero from '@/app/about/components/AboutHero'
+import Transform from '@/app/about/components/Transform'
+import Simplify from '@/app/about/components/Simplify'
+import Mission from '@/app/about/components/Mission'
+import Team from '@/app/about/components/Team'
+import Banner from '@/components/Banner'
+import Footer from '@/components/Footer'
 
 const page = () => {
   return (
-    <div>page</div>
+    <main>
+      <AboutHero />
+      <Transform />
+      <Simplify />
+      <Mission />
+      <Team />
+      <Banner />
+      <Footer />
+    </main>
   )
 }
 
