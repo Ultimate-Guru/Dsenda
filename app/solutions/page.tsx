2a39031ed72d7efbb1fb5662a0b28f7d2d041399
Oluwatosin <tosindaniel360@gmail.com>
2026-07-26 22:40:05 +0100

    feat: restructure solutions page layout with components for improved organization

--- app/solutions/page.tsx
+++ app/solutions/page.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import Solutionshero from "./components/Solutionshero"
+import Analytics from "@/app/solutions/components/Analytics"
+import WhyChooseUs from "./components/Whychooseus"
+import Faqsection from "./components/Faqsection"
+import Banner from "@/components/Banner"
+import Footer from "@/components/Footer"
 
 const page = () => {
   return (
-    <div>page</div>
+    <main>
+      <Solutionshero />
+      <Analytics />
+      <WhyChooseUs />
+      <Faqsection />
+      <Banner />
+      <Footer />
+    </main>
   )
 }
 
