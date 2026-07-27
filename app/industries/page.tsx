fb67680673aea91204826b5a99ceda013bcdbac7
Oluwatosin <tosindaniel360@gmail.com>
2026-07-27 15:00:58 +0100

    feat: update industries page layout to include IndustryHero, IndustryGrid, CapabilitySection, Banner, and Footer components

--- app/industries/page.tsx
+++ app/industries/page.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import IndustryHero from './comppnents/IndustryHero'
+import IndustryGrid from './comppnents/IndustryGrid'
+import CapabilitySection from './comppnents/CapabilitySection'
+import Banner from '@/components/Banner'
+import Footer from '@/components/Footer'
 
 const page = () => {
   return (
-    <div>page</div>
+    <main>
+      <IndustryHero />
+      <IndustryGrid />
+      <CapabilitySection />
+      <Banner />
+      <Footer />
+    </main>
   )
 }
 
